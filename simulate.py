14fa71a4a6f7e4e760abe67ccd08ef5d48b2819a
Terry Chen <chenterry85@gmail.com>
2022-07-10 17:59:15 -0400

    Add comments

--- simulate.py
+++ simulate.py
@@ -11,24 +11,26 @@ parser.add_argument("-v", "--volatility", default=0.1, type=float, help="The vol
 parser.add_argument("-p", "--paths", default=100, type=int, help="The desired number of generated paths")
 args = vars(parser.parse_args())
 
+# Set parameters
 initial_price = args['initial_price']
 drift = args['drift']
 volatility = args['volatility']
 n = args['paths']
 interval = 1/365. # set interval to one day
 
+# Initialize each path
 stochastic_processes = []
 for _ in range(n):
   stochastic_processes.append( GBM(initial_price,drift,interval,volatility) )
   
-# simulate each stochastic process
+# Simulate each stochastic process
 for process in stochastic_processes:
   tte = 1 # a year for time to expiry 
   while tte - process.dt > 0:
     process.time_step()
     tte -= process.dt
     
-# graph
+# Graphing
 for process in stochastic_processes:
   x = np.arange(len(process.asset_prices))
   y = process.asset_prices
